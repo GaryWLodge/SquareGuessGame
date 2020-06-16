e590225a27479bebc507e1cbb862eacd7c7e89a3
GaryWLodge <gwlgwl123@gmail.com>
2020-06-16 15:40:20 -0500

    card flip now working correct

--- src/App.js
+++ src/App.js
@@ -9,59 +9,37 @@ class App extends PureComponent {
 
   state = { 
     isFlipped: Array(4).fill(false),
-    shuffledCard: App.duplicateCard().sort(() => Math.random() - 0.5),
-    correctCard: Math.floor(Math.random() * 4 + 1),
+    cardArray: App.cardArray().sort(),
+    correctCard: Math.floor(Math.random() * (3 - 0) + 0),
     correctChoice: false
   };
 
-  static duplicateCard = () => {
-    return [1,2].reduce((preValue, current) => {
+  static cardArray = () => {
+    return [1,2].reduce((preValue, current, index) => {
       return preValue.concat([current, current])
     },[]);
   };
 
   handleClick = event => {
     event.preventDefault();
-    const cardId = event.target.id;
-    const selectedCard = this.state.isFlipped.slice();
+    const cardId = parseInt(event.target.id); 
+    const newFlipps = this.state.isFlipped.slice();
+    if (newFlipps[cardId] === false) {
+      newFlipps[cardId] = !newFlipps[cardId];
+      this.setState({ 
+        isFlipped: newFlipps,
+      })};
 
-    if (selectedCard[cardId] === this.state.correctCard) {
-       this.setState.correctChoice = true
+    if (cardId === this.state.correctCard) {
+       this.setState({ correctChoice: true})
 
-      if (this.state.clickCount === 2) {
-        this.setState({ clickCount: 1 });
-        const prevCardId = this.state.prevCardId;
-        const newCard = this.state.shuffledCard[cardId];
-        const previousCard = this.state.prevSelectedCard;
-
-        this.isCardCorrect(previousCard, newCard, prevCardId, cardId);
-        console.log(this.state)
-      }
     }
+    console.log(this.state)
+
   };
 
-  // isCardMatch = (card1, card2, card1Id, card2Id) => {
-  //   if (card1 === card2) {
-  //     const hideCard = this.state.shuffledCard.slice();
-  //     hideCard[card1Id] = -1;
-  //     hideCard[card2Id] = -1;
-  //     setTimeout(() => {
-  //       this.setState(prevState => ({
-  //         shuffledCard: hideCard
-  //       }))
-  //     }, 1000);
-  //   } else {
-  //     const flipBack = this.state.isFlipped.slice();
-  //     flipBack[card1Id] = false;
-  //     flipBack[card2Id] = false;
-  //     setTimeout(() => {
-  //       this.setState(prevState => ({ isFlipped: flipBack }));
-  //     }, 1000);
-  //   }
-  // };
-
-  isCardCorrect = (cardSelected, correctCard) => {
-    if (cardSelected === correctCard) {
+  isCardCorrect = () => {
+    if (this.state.correctChoice === true) {
       const displayCorrect = this.state.shuffledCard.slice();
       
     } else {
@@ -72,8 +50,7 @@ class App extends PureComponent {
   restartGame = () => {
     this.setState({
       isFlipped: Array(4).fill(false),
-      shuffledCard: App.duplicateCard().sort(() => Math.random() - 0.5),
-      correctCard: Math.floor(Math.random() * 4 + 1),
+      correctCard: App.correctCard().sort(() => Math.random() * 4 + 1),
       correctChoice: false
     });
   };
@@ -86,21 +63,22 @@ class App extends PureComponent {
     return (
      <div>
        <Header restartGame={this.restartGame} />
-       { this.isGameOver() ? <GameOver restartGame={this.restartGame} /> :
+       {/* { this.isGameOver() ? <GameOver restartGame={this.restartGame} /> : */}
        <div className="grid-container">
           {
-            this.state.shuffledCard.map((cardNumber, index) => 
+            this.state.cardArray.map((correctCard, index) => 
               <Card
                 key={index} 
                 id={index} 
-                cardNumber={cardNumber} 
+                correctCard={this.state.correctCard} 
                 isFlipped={this.state.isFlipped[index]} 
                 handleClick={this.handleClick}     
               />
             )
+            
           }
         </div>
-       }
+       
      </div>
     );
   }
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactCardFlip from "react-card-flip";
 
-const Card = ({ id, isFlipped, handleClick, cardNumber }) => (
+const Card = ({ id, isFlipped, handleClick, correctCard }) => (
   <ReactCardFlip isFlipped={isFlipped} flipSpeedBackToFront={1} flipSpeedFrontToBack={1} >
-    <button id={id} className={`card card-front ${cardNumber !== -1 ? "" : "hide-card"}`} onClick={handleClick} key="front">
-      
+    <button id={id} className={`card card-front ${correctCard === id ? "correct-card" : "wrong-card"}`} 
+    onClick={handleClick} key="front">
+      {id}
     </button>
 
-    <button id={id} className={`card card-back ${cardNumber !== -1 ? "" : "hide-card"}`} onClick={handleClick} key="back">
-      { cardNumber }
+    <button id={id} className={`card card-back ${correctCard === id ? "correct-card" : "wrong-card"}`} 
+    onClick={handleClick} key="back">
+      { correctCard }
     </button>
   </ReactCardFlip>
 );
