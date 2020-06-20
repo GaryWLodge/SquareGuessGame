a845de5bf61d866f20b2bf1dc72e2530e13723dc
GaryWLodge <gwlgwl123@gmail.com>
2020-06-20 17:06:53 -0500

    index.js added back

---
+++ build/precache-manifest.b3cabdae0a860c20275bef68c0fff242.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a536c7aa3ffb39107da39145eb768472",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c5fec8b77f45fa83314",
+    "url": "/static/css/main.ba5aa448.chunk.css"
+  },
+  {
+    "revision": "f18915f71f7737e4f931",
+    "url": "/static/js/2.0b62283e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b62283e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c5fec8b77f45fa83314",
+    "url": "/static/js/main.2932201f.chunk.js"
+  },
+  {
+    "revision": "a968ec14eae5fb5cc170",
+    "url": "/static/js/runtime-main.32898530.js"
+  }
+]);
